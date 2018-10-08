708b7a6f75cb4e2571f50c4cfcaad74a3153d9a8
Collins Agbonghama <collizo4sky@gmail.com>
2018-10-08 13:49:57 +0100

    If global optin is set, let never load rules also kick in. e.g. show everywhere except post a, B

--- src/OptinForms/PageTargetingRuleTrait.php
+++ src/OptinForms/PageTargetingRuleTrait.php
@@ -10,6 +10,7 @@ trait PageTargetingRuleTrait
      * Determine if the optin passes page level targeting rules.
      *
      * @param int $id
+     *
      * @return bool
      */
     public function page_level_targeting_rule_checker($id)
@@ -20,7 +21,7 @@ trait PageTargetingRuleTrait
          */
         $post_id = is_singular() || is_front_page() ? get_queried_object_id() : 0;
 
-        if (!$post_id) {
+        if ( ! $post_id) {
             if ('page' == get_option('show_on_front')) {
                 $post_id = get_option('page_for_posts');
             }
@@ -32,17 +33,34 @@ trait PageTargetingRuleTrait
         /** @var string $post_post_type post type of current post */
         $post_post_type = get_post_type($post_id);
 
-        if (OCR::get_customizer_value($id, 'click_launch_status') || OCR::get_merged_customizer_value($id, 'load_optin_globally')) {
-            return true;
-        } else {
-            $load_optin_index = OCR::get_customizer_value($id, 'load_optin_index');
-            $posts_never_load = OCR::get_customizer_value($id, 'posts_never_load');
-            $pages_never_load = OCR::get_customizer_value($id, 'pages_never_load');
-            $cpt_never_load = OCR::get_customizer_value($id, 'cpt_never_load');
-            $post_categories_load = OCR::get_customizer_value($id, 'post_categories_load');
-            $exclusive_post_types_posts_load = OCR::get_customizer_value($id, 'exclusive_post_types_posts_load');
-            $post_types_load = OCR::get_customizer_value($id, 'exclusive_post_types_load');
 
+        $load_optin_index                = OCR::get_customizer_value($id, 'load_optin_index');
+        $posts_never_load                = OCR::get_customizer_value($id, 'posts_never_load');
+        $pages_never_load                = OCR::get_customizer_value($id, 'pages_never_load');
+        $cpt_never_load                  = OCR::get_customizer_value($id, 'cpt_never_load');
+        $post_categories_load            = OCR::get_customizer_value($id, 'post_categories_load');
+        $exclusive_post_types_posts_load = OCR::get_customizer_value($id, 'exclusive_post_types_posts_load');
+        $post_types_load                 = OCR::get_customizer_value($id, 'exclusive_post_types_load');
+
+        if (OCR::get_customizer_value($id, 'click_launch_status') || OCR::get_merged_customizer_value($id, 'load_optin_globally')) {
+            // if current post should never contain optin, return false.
+            if ( ! empty($posts_never_load) && in_array($post_id, $posts_never_load)) {
+                return false;
+            }
+
+            // if current page should never contain optin, return false.
+            if ( ! empty($pages_never_load) && is_page($post_id) && in_array($post_id, $pages_never_load)) {
+                return false;
+            }
+
+            // if current CPT post should never contain optin, return false.
+            if ( ! empty($cpt_never_load) && in_array($post_id, $cpt_never_load)) {
+                return false;
+            }
+
+            return true;
+
+        } else {
             // if all targeting rules are empty, bail (do not show optin)
             if (empty($load_optin_index) &&
                 empty($posts_never_load) &&
@@ -61,28 +79,28 @@ trait PageTargetingRuleTrait
                 return false;
             }
             // if current view is neither frontpage, homepage, archive page or search page, return false.
-            if (!empty($load_optin_index) && (!(is_front_page() || is_home() || is_archive() || is_search()))) {
+            if ( ! empty($load_optin_index) && ( ! (is_front_page() || is_home() || is_archive() || is_search()))) {
                 return false;
             }
 
             // if current post should never contain optin, return false.
-            if (!empty($posts_never_load) && in_array($post_id, $posts_never_load)) {
+            if ( ! empty($posts_never_load) && in_array($post_id, $posts_never_load)) {
                 return false;
             }
 
             // if current page should never contain optin, return false.
-            if (!empty($pages_never_load) && is_page($post_id) && in_array($post_id, $pages_never_load)) {
+            if ( ! empty($pages_never_load) && is_page($post_id) && in_array($post_id, $pages_never_load)) {
                 return false;
             }
 
             // if current CPT post should never contain optin, return false.
-            if (!empty($cpt_never_load) && in_array($post_id, $cpt_never_load)) {
+            if ( ! empty($cpt_never_load) && in_array($post_id, $cpt_never_load)) {
                 return false;
             }
 
             // if current post category contain a category that optin should load for, return true.
             // array_intersect() return array element that exist in both comparison arrays.
-            if (!empty($post_categories_load)) {
+            if ( ! empty($post_categories_load)) {
                 $intersect = array_intersect($post_categories, $post_categories_load);
                 if (empty($intersect)) {
                     return false;
@@ -90,11 +108,11 @@ trait PageTargetingRuleTrait
             }
 
             // if current post isn't found in a set of all cpt posts to display optin for, return false.
-            if (!empty($exclusive_post_types_posts_load) && !in_array($post_id, $exclusive_post_types_posts_load)) {
+            if ( ! empty($exclusive_post_types_posts_load) && ! in_array($post_id, $exclusive_post_types_posts_load)) {
                 return false;
             }
 
-            if (!empty($post_types_load) && !in_array($post_post_type, $post_types_load)) {
+            if ( ! empty($post_types_load) && ! in_array($post_post_type, $post_types_load)) {
                 return false;
             }
         }
