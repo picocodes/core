c76ba374de88990cfe6bd6710b9f3714ee225add
Collins Agbonghama <collizo4sky@gmail.com>
2018-11-12 17:05:02 +0100

    branding is now hidden by default

--- src/Admin/Customizer/OptinForm/AbstractCustomizer.php
+++ src/Admin/Customizer/OptinForm/AbstractCustomizer.php
@@ -39,7 +39,7 @@ class AbstractCustomizer
     public function register_customizer_defaults()
     {
         $defaults                    = [];
-        $defaults['remove_branding'] = apply_filters('mo_optin_form_remove_branding_default', false, $this->customizer_defaults, $this->optin_campaign_type, $this->optin_campaign_class);
+        $defaults['remove_branding'] = apply_filters('mo_optin_form_remove_branding_default', true, $this->customizer_defaults, $this->optin_campaign_type, $this->optin_campaign_class);
 
         $defaults['form_background_image'] = apply_filters('mo_optin_form_background_image_default', '', $this->customizer_defaults, $this->optin_campaign_type, $this->optin_campaign_class);
         $defaults['form_image']            = apply_filters('mo_optin_form_image_default', '', $this->customizer_defaults, $this->optin_campaign_type, $this->optin_campaign_class);
