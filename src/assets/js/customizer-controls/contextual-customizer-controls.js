7619cae0cbea1ba0cb5a700a61d2633609935331
Collins Agbonghama <collizo4sky@gmail.com>
2019-02-04 19:31:29 +0100

    Improved contextual panel display form customizer

--- src/assets/js/customizer-controls/contextual-customizer-controls.js
+++ src/assets/js/customizer-controls/contextual-customizer-controls.js
@@ -125,6 +125,12 @@
                     api.section('mo_wp_x_page_views_display_rule_section').active(!is_displayed());
                     api.section('mo_wp_page_filter_display_rule_section').active(!is_displayed());
                     api.section('mo_wp_shortcode_template_tag_display_rule_section').active(!is_displayed());
+                    api.section('mo_wp_adblock_display_rule_section').active(!is_displayed());
+                    api.section('mo_wp_newvsreturn_display_rule_section').active(!is_displayed());
+                    api.section('mo_wp_referrer_detection_display_rule_section').active(!is_displayed());
+                    api.section('mo_wp_schedule_display_rule_section').active(!is_displayed());
+                    api.section('mo_wp_device_targeting_display_rule_section').active(!is_displayed());
+                    api.section('mo_wp_user_filter_display_rule_section').active(!is_displayed());
                 };
 
                 control.active.validate = is_displayed;
